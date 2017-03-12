d2cea2eeb680aa70e681af96afd1f37200e749ac
Emil Bay <github@tixz.dk>
2017-03-12 10:30:32 +0100

    Add crypto_pwhash_*

--- README.md
+++ README.md
@@ -18,8 +18,36 @@ var random = sodium.randombytes_buf(Buffer.allocUnsafe(16))
 
 See [`sodium-native`](https://github.com/mafintosh/sodium-native) for complete documentation.
 
-### `var output = sodium.randombytes_buf(output)`
+### `sodium.randombytes_*`
+
+#### `var output = sodium.randombytes_buf(output)`
 `output` can either be a Buffer or integer length. Will return `output`
+
+### `sodium.crypto_pwhash_*`
+
+**Constants**:
+
+- `sodium.crypto_pwhash_ALG_ARGON2I13`
+- `sodium.crypto_pwhash_SALTBYTES`
+- `sodium.crypto_pwhash_STRBYTES`
+- `sodium.crypto_pwhash_STRPREFIX`
+- `sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE`
+- `sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE`
+- `sodium.crypto_pwhash_OPSLIMIT_MODERATE`
+- `sodium.crypto_pwhash_MEMLIMIT_MODERATE`
+- `sodium.crypto_pwhash_OPSLIMIT_SENSITIVE`
+- `sodium.crypto_pwhash_MEMLIMIT_SENSITIVE`
+- `sodium.crypto_pwhash_PRIMITIVE`
+
+
+#### `var output = sodium.crypto_pwhash(output, password, salt, opslimit, memlimit, algorithm)`
+`output` can either be a Buffer or integer length. Will return `output`
+
+#### `var output = sodium.crypto_pwhash_str([output,] password, opslimit, memlimit)`
+`output` is optional, but can be Buffer or integer length. Defaults to Buffer of `sodium.crypto_pwhash_STRBYTES` length. Will return `output`
+
+#### `var bool = sodium.crypto_pwhash_str_verify(str, password)`
+Alias to [`sodium-native#crypto_pwhash_str_verify`](https://github.com/mafintosh/sodium-native#var-bool--crypto_pwhash_str_verifystr-password)
 
 ## Install
 
--- index.js
+++ index.js
@@ -13,3 +13,50 @@ exports.randombytes_buf = function (output) {
 
   return output
 }
+
+// pwhash
+
+exports.crypto_pwhash_ALG_ARGON2I13 = sodium.crypto_pwhash_ALG_ARGON2I13
+exports.crypto_pwhash_SALTBYTES = sodium.crypto_pwhash_SALTBYTES
+exports.crypto_pwhash_STRBYTES = sodium.crypto_pwhash_STRBYTES
+exports.crypto_pwhash_STRPREFIX = sodium.crypto_pwhash_STRPREFIX
+exports.crypto_pwhash_OPSLIMIT_INTERACTIVE = sodium.crypto_pwhash_OPSLIMIT_INTERACTIVE
+exports.crypto_pwhash_MEMLIMIT_INTERACTIVE = sodium.crypto_pwhash_MEMLIMIT_INTERACTIVE
+exports.crypto_pwhash_OPSLIMIT_MODERATE = sodium.crypto_pwhash_OPSLIMIT_MODERATE
+exports.crypto_pwhash_MEMLIMIT_MODERATE = sodium.crypto_pwhash_MEMLIMIT_MODERATE
+exports.crypto_pwhash_OPSLIMIT_SENSITIVE = sodium.crypto_pwhash_OPSLIMIT_SENSITIVE
+exports.crypto_pwhash_MEMLIMIT_SENSITIVE = sodium.crypto_pwhash_MEMLIMIT_SENSITIVE
+exports.crypto_pwhash_PRIMITIVE = sodium.crypto_pwhash_PRIMITIVE
+
+// var output = crypto_pwhash(output, password, salt, opslimit, memlimit, algorithm)
+exports.crypto_pwhash = function (output, password, salt, opslimit, memlimit, algorithm) {
+  if (Number.isSafeInteger(output)) {
+    output = Buffer.allocUnsafe(output)
+  }
+
+  sodium.crypto_pwhash(output, password, salt, opslimit, memlimit, algorithm)
+
+  return output
+}
+
+// var output = crypto_pwhash_str([output,] password, opslimit, memlimit)
+exports.crypto_pwhash_str = function (output, password, opslimit, memlimit) {
+  if (memlimit == null) {
+    return exports.crypto_pwhash_str(null, output, password, opslimit)
+  }
+
+  if (output == null) {
+    output = exports.crypto_pwhash_STRBYTES
+  }
+
+  if (Number.isSafeInteger(output)) {
+    output = Buffer.allocUnsafe(output)
+  }
+
+  sodium.crypto_pwhash_str(output, password, opslimit, memlimit)
+
+  return output
+}
+
+// var bool = crypto_pwhash_str_verify(str, password)
+exports.crypto_pwhash_str_verify = sodium.crypto_pwhash_str_verify
